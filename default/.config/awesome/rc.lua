1567364461c6ae4723ce6e2c866ca8cf3927819f
James Bucher <james@JamesBucher-LT.(none)>
2011-06-17 22:46:25 -0700

    Add key binding to switch escape and capslock under X11

--- default/.config/awesome/rc.lua
+++ default/.config/awesome/rc.lua
@@ -340,3 +340,4 @@ client.add_signal("unfocus", function(c) c.border_color = beautiful.border_norma
 
 --Autostart Applications
 awful.util.spawn_with_shell("~/.bin/dex -a")
+awful.util.spawn_with_shell("xmodmap ~/.config/speedswapper")
