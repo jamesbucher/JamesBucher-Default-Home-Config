03c227ed4ef808be215cec3988c915fd09b4565e
James Edward Bucher <bucherj@CF498-xx.cs.wwu.edu>
2012-02-20 20:54:59 -0800

    Fix .emacs shit

--- default/.emacs.d/init.el
+++ default/.emacs.d/init.el
@@ -76,4 +76,27 @@
   (mapc #'load (directory-files user-specific-dir nil ".*el$")))
 (if (file-exists-p user-specific-config) (load user-specific-config))
 
+;;; Set tab spacing
+(setq-default c-basic-offset 4)
+(setq-default c-default-style '((java-mode . "java") (awk-mode . "awk") (c-mode . "k&r") (c++-mode . "k&r") (other . "gnu")))
+
+;;; Set Evil (Vim emulation)
+(add-to-list 'load-path "~/.emacs.d/evil")
+(require 'evil)
+(evil-mode 1)
+
+;;; Remove startup screen
+(setq inhibit-startup-screen t)
+
+
+;;; This was installed by package-install.el.
+;;; This provides support for the package system and
+;;; interfacing with ELPA, the package archive.
+;;; Move this code earlier if you want to reference
+;;; packages in your .emacs.
+(when
+    (load
+     (expand-file-name "~/.emacs.d/elpa/package.el"))
+  (package-initialize))
+
 ;;; init.el ends here
