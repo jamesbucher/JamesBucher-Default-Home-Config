6c405cdf7c804c81110ddfe8f11659510456eb29
James Edward Bucher <bucherj@CF498-xx.cs.wwu.edu>
2012-02-20 22:00:47 -0800

    blarg

--- default/.emacs.d/init.el
+++ default/.emacs.d/init.el
@@ -101,12 +101,12 @@
 
 
 ;;; Add color-theme for solarized
-;(add-to-list 'load-path "~/.emacs.d/color-theme/color-theme.el")
-;(require 'color-theme)
-;(eval-after-load "color-theme"
-;'(progn
-;   (color-theme-initialize)
-;   (color-theme-hober)))
+(add-to-list 'load-path "~/.emacs.d/color-theme/color-theme.el")
+(require 'color-theme)
+(eval-after-load "color-theme"
+'(progn
+   (color-theme-initialize)
+   (color-theme-hober)))
 
 ;;; Actual solarized theme
 (add-to-list 'load-path "~/.emacs.d/color-theme-solarized")
